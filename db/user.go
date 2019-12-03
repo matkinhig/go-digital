9816a460a75f3572b0617ae447ce228eeccc7e0c
matkinhig <matkinhig@gmail.com>
2019-12-03 17:08:50 +0700

    commit to edit types querry

--- db/user.go
+++ db/user.go
@@ -1,10 +1,33 @@
 package db
 
-func GetAllUsers() (*[]types.EUser, error) {
+import (
+	"fmt"
+	"log"
+
+	"github.com/matkinhig/go-digital/config"
+	"github.com/matkinhig/go-digital/types"
+)
+
+func GetAllUsers() (*[]types.EUsers{}, error) {
+	fmt.Println("Start querry all users")
 	db, err := config.InitDB()
 	if err != nil {
 		panic(err)
 	}
-	row, err := db.Querry
-	sUsers := []types.EUser{}
+	rows, err := db.Query("select * from halong.ibkeusers")
+	if err != nil {
+		panic(err)
+	}
+	sUsers := []types.EUsers{}
+	for rows.Next() {
+		var us types.EUsers
+		err = rows.Scan(&us)
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+		sUsers = append(sUsers, us)
+	}
+	fmt.Println(sUsers[1])
+	return sUsers, err
 }
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/matkinhig/go-digital/config"
+	"github.com/matkinhig/go-digital/db"
 )
 
 func main() {
 	fmt.Println("Start Golang")
 	fmt.Println(config.Config)
+	s, err := db.GetAllUsers()
+	fmt.Println(err)
+	fmt.Println(s)
 }
